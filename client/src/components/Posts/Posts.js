720ad912c21ff48ef035cd58e958262991a6aa91
hyunsang-ahn <happylovetkd@naver.com>
2022-08-24 07:05:58 +0900

    pjt day5

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Post from './Post/Post'
 import useStyles from './styles'
 
-
 function Posts() {
   const classes = useStyles()
+  const posts = useSelector((state) => state.posts)
+
+  console.log('posts============', posts)
   return (
     <div>
       <Post />
--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ import cors from 'cors'
 import postRoutes from './routes/posts.js'
 const app = express()
 
-app.use('/posts', postRoutes)
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
-
+app.use('/posts', postRoutes)
 
 const CONNETION_URL = 'mongodb+srv://admin:ReW86vMoeO7nhltT@cluster0.avuqeyr.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
