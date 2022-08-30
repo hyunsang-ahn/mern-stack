37756daf7304f80ac1a9a77bd77665047ca73adf
hyunsang-ahn <happylovetkd@naver.com>
2022-08-31 07:43:36 +0900

    auth error fixing...

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from './styles'
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'
-import Navbar from './components/Navbar/Navbar';
+import React from 'react'
+import { Container } from '@material-ui/core';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
+import Auth from './components/Auth/Auth';
 
 
 const App = () => {
-  const classes = useStyles()
-  const dispatch = useDispatch();
-  const [currentId, setCurrentId] = useState(null)
-  useEffect(() => {
-    dispatch(getPosts())
-  }, [currentId, dispatch])
+
   return (
-    <Container maxWidth='lg'>
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={4}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={5}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
+    <Router>
 
-            </Grid>
+      <Container maxWidth='lg'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
 
-          </Grid>
-
-        </Container>
-      </Grow>
-    </Container>
+        </Routes>
+      </Container>
+    </Router>
   )
 }
 
