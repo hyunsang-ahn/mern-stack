4595f4f492647dbbf63b80cb4347a1c4a07bfbf8
hyunsang-ahn <happylovetkd@naver.com>
2022-09-22 06:49:00 +0900

    pagination 기능 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { Container } from '@material-ui/core';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-
 import Auth from './components/Auth/Auth';
+import PostDetail from './components/PostDetail/PostDetail';
+
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
 const App = () => {
-
+  const user = JSON.parse(localStorage.getItem('profile'))
   return (
     <GoogleOAuthProvider clientId="66618411565-2j4jhti4j7skhnql19rsvbs2fk9uvhkc.apps.googleusercontent.com">
 
       <Router>
 
-        <Container maxWidth='lg'>
+        <Container maxWidth='xl'>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<Navigate to="/posts" />} />
+            <Route path="/posts" element={<Home />} />
+            <Route path="/posts/search" element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+
+            <Route path="/auth" element={(!user ? <Auth /> : <Navigate to="/posts" />)} />
 
           </Routes>
         </Container>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,9 +3,11 @@ import React, { useState, useEffect } from 'react'
 import useStyles from './styles'
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+
+import Paginate from '../Pagination';
 const Home = () => {
   console.log('this is home============')
   const classes = useStyles()
@@ -23,7 +25,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={5}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
-
+            <Paper className={classes.paginztion} elevation={6}>
+              <Paginate />
+            </Paper>
           </Grid>
 
         </Grid>
