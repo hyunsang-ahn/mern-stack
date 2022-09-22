ebe35bc9f6e0ce87b6644cbf6f8b6de3337f3724
hyunsang-ahn <happylovetkd@naver.com>
2022-09-23 06:58:57 +0900

    search 기능 생성

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -16,6 +16,18 @@ export const getPosts = () => async (dispatch) => {
 
 
 
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPostsBySearch(searchQuery);
+    console.log(data)
+    // dispatch({ type: FETCH_ALL, payload: data })
+
+  } catch (error) {
+    console.log(error.message)
+  }
+
+}
+
 
 
 
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,12 +2,21 @@ import React, { useState, useEffect } from 'react'
 
 import useStyles from './styles'
 import { useDispatch } from 'react-redux';
-import { getPosts } from '../../actions/posts'
-import { Container, AppBar, Typography, Grow, Grid, Paper } from '@material-ui/core';
+import { useHistory, useLocation } from 'react-router-dom'
+import ChipInput from 'material-ui-chip-input'
+import { getPosts, getPostsBySearch } from '../../actions/posts'
+
+import { Container, AppBar, Typography, Grow, Grid, Paper, TextField, Button } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 
 import Paginate from '../Pagination';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search)
+}
+
+
 const Home = () => {
   console.log('this is home============')
   const classes = useStyles()
@@ -16,14 +25,71 @@ const Home = () => {
   useEffect(() => {
     dispatch(getPosts())
   }, [currentId, dispatch])
+  const query = useQuery();
+  // const history = useHistory()
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery')
+  const [search, setSearch] = useState('')
+  const [tags, setTags] = useState([])
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      //enter
+      searchPost()
+    }
+
+  }
+  const handleAdd = (tag) => {
+    setTags([...tags, tag])
+  }
+  const handleDelete = (tagToDelete) => {
+    setTags(tags.filter((tag) => tag !== tagToDelete))
+
+  }
+
+  const searchPost = () => {
+    if (search.trim()) {
+      //dispatch -> fetch search post
+      dispatch(getPostsBySearch({
+        search, tags: tags.join(',')
+      }))
+    } else {
+      history.pushState('/')
+    }
+  }
   return (
     <Grow in>
-      <Container>
-        <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={4}>
-          <Grid item xs={12} sm={7}>
+      <Container maxWidth='xl'>
+        <Grid className={classes.gridContainer} container justifyContent="space-between" alignItems="stretch" spacing={4}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={5}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+              <TextField
+                name="search"
+                variant='outlined'
+                label=" Search Memories"
+                fullwidth value="Test"
+                onChange={(e) => (setSearch(e.target.value))}
+                onKeyPress={handleKeyPress}
+              >
+
+              </TextField>
+              <ChipInput
+                style={{ margin: '10px' }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label='search tags'
+                variant='outlined'
+              >
+
+              </ChipInput>
+              <Button variant="contained" onClick={searchPost} className={classes.searchButton} color="primary">
+                search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper className={classes.paginztion} elevation={6}>
               <Paginate />
